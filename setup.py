79a782682e3a0cf448cdc9843479e46481f9fa98
Arnaud Grausem <arnaud.grausem@unistra.fr>
2015-11-19 17:32:48 +0100

    new minor version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ recursive_requirements('requirements.txt', libraries, dependency_links)
 
 setup(
     name = "django-workflow-activity",
-    version = "1.0.6",
+    version = "1.0.7",
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
 
     install_requires=libraries,
